28e5ec51cc0758d584800869eeea6e46f129281c
Fransditoooo <fransdito.bayu.ft24@mail.umy.ac.id>
2025-05-05 11:12:33 +0700

    class siswa (friend class satu)

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -12,3 +12,20 @@ class orang
     void setNama(string pNama);
     friend class siswa;
 };
+
+    class siswa
+    {
+        private:
+        int id;
+
+        public:
+        void setId(int pId);
+        void displayAll(orang &a);
+    };
+
+    void siswa::displayAll(orang &a)
+    {
+        cout << id << endl << a.nama;
+    }
+
+    
